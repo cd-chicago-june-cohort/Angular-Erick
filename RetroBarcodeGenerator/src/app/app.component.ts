4386b3217eed9fd40c20d485912cd794c64c0c3c
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-08-10 15:34:20 -0500

    awesome barcode generator

---
+++ RetroBarcodeGenerator/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  colorArray = ['Red', 'DarkSeaGreen', 'LightSkyBlue', 'Orchid', 'Plum', 'PaleTurquoise', 'PaleVioletRed', 'RosyBrown', 'RoyalBlue', 'SteelBlue']
+  random = [
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+    Math.floor(Math.random() * 9) + 1,
+  ]
+}
